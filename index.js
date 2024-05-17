5359d2d9fc1bebc1dd4837c1264ea46df09bcb0f
shawon13 <rakibarg1519@gmail.com>
2024-05-17 10:04:00 +0600

    product filtering by id

--- index.js
+++ index.js
@@ -37,6 +37,11 @@ app.get('/categories/:categoryId', (req, res) => {
     const categoryId = products.filter(c => c.category_id == id);
     res.send(categoryId)
 })
+app.get('/categories/:id', (req, res) => {
+    const id = req.params.id;
+    const productId = products.filter(p => p.id == id);
+    res.send(productId)
+})
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
